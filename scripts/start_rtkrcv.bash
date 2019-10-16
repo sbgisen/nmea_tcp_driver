be11c9601601a86969c934628c5e3ee6bb28284a
ashTape <shotaro.akima01@g.softbank.co.jp>
2019-10-16 14:01:36 +0900

    [add] Add files for usage of NEO-M8T by RTKLIB rtkrcv

---
+++ scripts/start_rtkrcv.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+CMDNAME=`basename $0`
+
+RTKLIB_PATH=$HOME/workspace/RTKLIB
+PACKAGE_PATH=$HOME/ros/src/ublox/ublox_gps
+BASE_NAME="CQ"
+
+while getopts b:p:r: OPT
+do
+    case $OPT in
+        "b" ) BASE_NAME="$OPTARG"; echo "Base station : $BASE_NAME" ;;
+        "p" ) PACKAGE_PATH="$OPTARG" ; echo "Package path : $PACKAGE_PATH" ;;
+        "r" ) RTKLIB_PATH="$OPTARG"; echo "RTKLIB path : $RTKLIB_PATH" ;;
+          * ) echo "Usage : $CMDNAME [-d VALUE] [-p VALUE} [-s VALUE]"
+    esac
+done
+
+echo "Base station : $BASE_NAME"
+echo "Package path : $PACKAGE_PATH" 
+echo "RTKLIB path : $RTKLIB_PATH"
+
+$RTKLIB_PATH/app/rtkrcv/gcc/rtkrcv -o $PACKAGE_PATH/rtklib_config/rtk_$BASE_NAME.conf  
